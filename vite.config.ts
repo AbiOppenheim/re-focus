8ae7de5b469dccf19f9296a0e760c6850b3cd4c6
AbiOppenheim <abi@reachlatent.com>
2025-11-18 17:38:38 -0300

    chore: trigger deployment

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, loadEnv } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig(({ mode }) => {
+  // Trigger deployment update
   const env = loadEnv(mode, '.', '');
   const repoName = 're-focus';
   return {
